72b2e9e839c6e67850b425fae9780f07365c5f31
IlyaLavern <65504519+IlyaLavern@users.noreply.github.com>
2021-03-18 20:39:48 +0200

    Лабораторна 1 ТСПП

---
+++ Лаб 1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApplication35
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введіть тризначне число X");
+            int x = int.Parse(Console.ReadLine());
+            int first, mid, end;
+            first = x / 100;
+            end = x % 10;
+            mid = ((x % 100) / 10);
+            int result = (end * 100) + (mid * 10) + first;
+            Console.WriteLine("Отримане тризначне число: {0}", result);
+        }
+    }
+}
