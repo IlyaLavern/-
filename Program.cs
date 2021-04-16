84e089e13730b24aeeca5227e16669efa6d29927
IlyaLavern <65504519+IlyaLavern@users.noreply.github.com>
2021-04-16 19:06:10 +0300

    Лабораторна1

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Лабораторна1
+{
+    public class Program
+    {
+        static void Main()
+        {
+            string text;
+            do
+            {
+                Console.WriteLine("Введіть тризначне число X");
+                Console.Write("X = ");
+                text = Console.ReadLine();
+                bool ok = int.TryParse(text, out int x);
+                if (ok && text.Length == 3)
+                {
+                    Console.WriteLine("\nОтримане тризначне число: {0}\n", Mirror(x));
+                    
+                }
+                else if (text != "")
+                {
+                    Console.WriteLine("\nПотрібно ввести тризначне число X\n");
+                }
+            } 
+        }
+
+        public static int Mirror(int x)
+        {
+            int first, mid, end;
+            first = x / 100;
+            end = x % 10;
+            mid = ((x % 100) / 10);
+            return (end * 100) + (mid * 10) + first;
+        }
+    }
+}
