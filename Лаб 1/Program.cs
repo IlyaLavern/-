431d405789b47ef246cae95b1e2671c99775429d
IlyaLavern <65504519+IlyaLavern@users.noreply.github.com>
2021-04-16 19:20:37 +0300

    Update Program.cs

--- Лаб 1/Program.cs
+++ Лаб 1/Program.cs
@@ -1,24 +1,7 @@
-﻿using System;
 
-namespace ConsoleApplication35
-{
-    class Program
-    {
-        static void Main()
-        {
-            Console.WriteLine("Введіть тризначне число X");
-            int x = int.Parse(Console.ReadLine());
-            int first, mid, end;
-            first = x / 100;
-            end = x % 10;
-            mid = ((x % 100) / 10);
-            int result = (end * 100) + (mid * 10) + first;
-            Console.WriteLine("Отримане тризначне число: {0}", result);
-        }
-    }
-}﻿using System;
+using System;
 
-namespace ConsoleApplication35
+namespace Лабораторна1
 {
     class Program
     {
